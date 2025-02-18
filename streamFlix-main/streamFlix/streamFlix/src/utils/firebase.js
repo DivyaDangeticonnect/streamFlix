2bf166cf7e553bbbd84cbfb2aac12db9821d8895
Teja Lakshmi Divya Dangeti <divyadangeti789@gmail.com>
2025-02-18 22:10:47 +0530

    Update firebase.js

--- streamFlix-main/streamFlix/streamFlix/src/utils/firebase.js
+++ streamFlix-main/streamFlix/streamFlix/src/utils/firebase.js
@@ -8,13 +8,6 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBP5OkB828MDt3rnXg6kzrRBtYXJu9ooxQ",
-  authDomain: "streamflix-216ca.firebaseapp.com",
-  projectId: "streamflix-216ca",
-  storageBucket: "streamflix-216ca.appspot.com",
-  messagingSenderId: "822091700241",
-  appId: "1:822091700241:web:a14c9d17b707b83fc239b0",
-  measurementId: "G-4GENSV0RBE"
 };
 
 // Initialize Firebase
